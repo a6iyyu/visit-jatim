484c14e165243eda3ded4087daf31b1f5af72c6a
a6iyyu <ffst4irs@gmail.com>
2024-02-12 12:14:03 +0700

    Building the Pendidikan page.

--- src/layouts/homepage.vue
+++ src/layouts/homepage.vue
@@ -37,7 +37,7 @@ import CardCityVue from "../components/CardCity.vue";
         <a href="https://www.jawapos.com/wisata-kuliner/011771965/8-desa-wisata-terbaik-di-jawa-timur-yang-wajib-dikunjungi"
           class="h-full w-full block">
           <div class="h-80 w-full rounded-md bg-bottom bg-cover bg-no-repeat" :style="{
-            backgroundImage: 'url(../../static/img/sample-1.jpg)',
+            backgroundImage: 'url(/static/img/sample-1.jpg?url)',
             boxShadow: '0.75rem 0.75rem 0.75rem #bebebe',
           }"></div>
           <br />
