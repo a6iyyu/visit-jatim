d8ad5b8a32dc992fdae73cb0003d14e86e43ab40
a6iyyu <ffst4irs@gmail.com>
2024-02-25 00:02:56 +0700

    Deploy?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import react from "@astrojs/react";
 import partytown from "@astrojs/partytown";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [tailwind(), vue(), react(), partytown()],
   server: {
     port: 5000,
   },
+  output: "server",
+  adapter: vercel(),
 });
