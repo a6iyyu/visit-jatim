7c17ddfe2fb6b8d845fb48280804d1d23eb27551
Afif-Ghifari <aghifari1204@gmail.com>
2024-03-03 13:33:17 +0700

    test1.2.4

--- src/components/CardCity.vue
+++ src/components/CardCity.vue
@@ -1,12 +1,7 @@
 <template>
-  <a
-    :href="city.link"
-    v-for="(city, index) in cities"
-    :key="index"
+  <a :href="city.link" v-for="(city, index) in cities" :key="index"
     class="splide__slide h-full w-full flex flex-col justify-end rounded-3xl bg-center bg-cover bg-no-repeat"
-    :style="{ backgroundImage: 'url(' + city.img + ')' }"
-    target="_blank"
-  >
+    :style="{ backgroundImage: `url(${city.img})` }" target="_blank">
     <div class="h-2/5 w-full flex flex-col items-center justify-center z-40">
       <h3 class="text-4xl font-black transition-all duration-300 ease-in-out text-white hover:text-slate-300">
         {{ city.city }}
