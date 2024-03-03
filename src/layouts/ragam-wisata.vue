55ac6846e2ad585a044661ce7be45143415dc9fe
Afif-Ghifari <aghifari1204@gmail.com>
2024-03-03 11:45:35 +0700

    test1.1.3

--- src/layouts/ragam-wisata.vue
+++ src/layouts/ragam-wisata.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { ref, watch, onMounted } from "vue";
 import destination from "../data/destination.json";
-import hotel from '/static/img/hotel-majapahit.jpg';
+import hotel from '../../static/img/hotel-majapahit.jpg';
 
 const destinations = ref(destination.places);
 // const arrayDestination = ref([...destinations.value].sort(() => Math.random() - 0.5).slice(0, 6));
@@ -51,7 +51,7 @@ watch(selectCity, (newSelectCity: string) => {
 
     <!-- Hero Mobile -->
     <div class="h-full w-3/5 flex items-end justify-center bg-center bg-cover bg-no-repeat"
-      :style="{ backgroundImage: `url(${hotel.src})` }">
+      :style="{ backgroundImage: `url(${hotel})` }">
       <div
         class="block md:hidden absolute min-w-full min-h-full bg-gradient-to-t from-black opacity-50 hover:opacity-80 transition-opacity duration-100 z-20">
       </div>
