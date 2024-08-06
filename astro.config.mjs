c72ed071476dc238070988d72ef28433a392147e
ffst4irs@gmail.com <ffst4irs@gmail.com>
2024-08-06 08:06:55 +0700

    :arrow_up: Upgrade dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
-import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [vue(), tailwind(), mdx(), react()],
+  integrations: [tailwind(), mdx(), react()],
   server: {
     port: 5000,
   },
